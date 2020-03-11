73bac59ef5038445aba96da69fb9c3edb84a8103
ermekaitygulov <ermek.imankulov@gmail.com>
2020-03-11 14:28:44 +0300

    trying to fix bug with gripper

--- pyrep_test.py
+++ pyrep_test.py
@@ -1,9 +1,9 @@
 from environments.pyrep_env import RozumEnv
 import timeit
-
-from utils.wrappers import DiscreteWrapper
+from utils.wrappers import DiscreteWrapper, SaveVideoWrapper
 
 env = RozumEnv()
+env = SaveVideoWrapper(RozumEnv)
 done = False
 start_time = timeit.default_timer()
 discrete_dict = dict()
@@ -13,13 +13,14 @@ for i in range(robot_dof):
     discrete_dict[i + robot_dof] = [-5 if j == i else 0 for j in range(robot_dof)]
 env = DiscreteWrapper(env, discrete_dict)
 
-for i in range(300):
+for i in range(100):
     action = env.sample_action()
     state, reward, done, _ = env.step(action)
     print("State shape: ", state.shape)
     print("Reward: ", reward)
     if i % 100 == 0:
         env.reset()
+env.reset()
 
 stop_time = timeit.default_timer()
 print("RunTime: ", stop_time - start_time)
