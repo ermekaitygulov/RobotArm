18814af851a767657cde6e7d97bbd88d2313ca5b
ermekaitygulov <ermek.imankulov@gmail.com>
2020-03-22 10:52:37 +0300

    debugging apex

--- replay_buffers/apex_buffer.py
+++ replay_buffers/apex_buffer.py
@@ -7,6 +7,9 @@ import ray
 
 @ray.remote
 class ApeXBuffer(PrioritizedBuffer):
+    def len(self):
+        return len(self.tree)
+
     def receive(self, transitions, priorities):
         for t, p in zip(transitions, priorities):
             self.tree.add(p, t)
