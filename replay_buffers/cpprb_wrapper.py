918699c8b0e1b7cfb746ec157a193ff038139225
ermekaitygulov <ermek.imankulov@gmail.com>
2020-04-25 15:40:50 +0300

    add timer to buffer

--- replay_buffers/cpprb_wrapper.py
+++ replay_buffers/cpprb_wrapper.py
@@ -1,4 +1,6 @@
 from cpprb import PrioritizedReplayBuffer, ReplayBuffer
+import timeit
+from collections import deque
 
 
 class PER(PrioritizedReplayBuffer):
@@ -7,25 +9,43 @@ class PER(PrioritizedReplayBuffer):
         super(PER, self).__init__(*args, **kwargs)
         self.state_prefix = state_prefix
         self.state_keys = state_keys
+        self.add_deque = deque(maxlen=10)
+        self.sample_deque = deque(maxlen=10)
+        self.update_deque = deque(maxlen=10)
 
     def sample(self, *args, **kwargs):
+        start_time = timeit.default_timer()
         batch = super(PER, self).sample(*args, **kwargs)
         for prefix in self.state_prefix:
             batch[prefix+'state'] = {key: batch.pop(prefix+key) for key in self.state_keys}
+        stop_time = timeit.default_timer()
+        self.sample_deque.append(stop_time - start_time)
+        print("Sample time (it/sec): {:.2f}".format(len(self.sample_deque)/sum(self.sample_deque)))
         return batch
 
     def add(self, **kwargs):
+        start_time = timeit.default_timer()
         for prefix in self.state_prefix:
             state = kwargs.pop(prefix+'state')
             for key, value in state.items():
                 kwargs[prefix + key] = value
         super(PER, self).add(**kwargs)
+        stop_time = timeit.default_timer()
+        self.add_deque.append(stop_time - start_time)
+        print("Add time (it/sec): {:.2f}".format(len(self.add_deque) / sum(self.add_deque)))
 
     def get_all_transitions(self):
         batch = super(PER, self).get_all_transitions()
         for prefix in self.state_prefix:
             batch[prefix + 'state'] = {key: batch.pop(prefix + key) for key in self.state_keys}
         return batch
+
+    def update_priorities(self, *args, **kwargs):
+        start_time = timeit.default_timer()
+        super(PER, self).update_priorities(*args, **kwargs)
+        stop_time = timeit.default_timer()
+        self.update_deque.append(stop_time - start_time)
+        print("Update time (it/sec): {:.2f}".format(len(self.update_deque) / sum(self.update_deque)))
 
 
 class RB(ReplayBuffer):
