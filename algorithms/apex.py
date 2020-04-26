a721ad458c127a62c30bbf96ea505d28e0f2d819
ermekaitygulov <ermek.imankulov@gmail.com>
2020-04-26 12:58:36 +0300

    remove preschedule; try small batch

--- algorithms/apex.py
+++ algorithms/apex.py
@@ -18,10 +18,6 @@ class Learner(DQN):
                          log_freq=log_freq)
         self.summary_writer = tf.summary.create_file_writer('train/learner/')
 
-    def preshedule(self, sample):
-        self.schedule()
-        return sample
-
     def update_from_ds(self, ds, start_time, batch_size):
         import tensorflow as tf
 
@@ -30,13 +26,13 @@ class Learner(DQN):
         ds = tf.data.Dataset.from_tensor_slices(ds)
         ds = ds.batch(batch_size)
         ds = ds.map(self.preprocess_ds)
-        ds = ds.map(self.preshedule)
         ds = ds.cache()
         ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
         for batch in ds:
             _, ntd_loss, _, _ = self.q_network_update(gamma=self.gamma, **batch)
             stop_time = timeit.default_timer()
             self._run_time_deque.append(stop_time - start_time)
+            self.schedule()
             loss_list.append(ntd_loss)
             start_time = timeit.default_timer()
         return indexes, np.abs(np.concatenate(loss_list))
