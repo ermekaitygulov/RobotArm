200943b11f0024e7e2a5548d425e15ee2a89033a
ermekaitygulov <ermek.imankulov@gmail.com>
2020-04-29 11:46:34 +0300

    start ddpg run

---
+++ algorithms/ddpg/run.py
@@ -0,0 +1,79 @@
+from algorithms.ddpg.ddpg import DDPG
+from common.cpprb_wrapper import PER
+from algorithms.model import ClassicCnn, DuelingModel, MLP
+from environments.pyrep_env import RozumEnv
+from common.wrappers import *
+import tensorflow as tf
+import os
+from common.util import config_gpu
+
+
+def ddpg_run():
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+    tf.config.optimizer.set_jit(True)
+    config_gpu()
+
+    env = RozumEnv()
+    # env = SaveVideoWrapper(env)
+    env = FrameSkip(env)
+    env = FrameStack(env, 2, stack_key='pov')
+    env = AccuracyLogWrapper(env, 10)
+    env_dict = {'action': {'dtype': 'float32'},
+                'reward': {'dtype': 'float32'},
+                'done': {'dtype': 'bool'},
+                'n_reward': {'dtype': 'float32'},
+                'n_done': {'dtype': 'bool'},
+                'actual_n': {'dtype': 'float32'}}
+    for prefix in ('', 'n_'):
+        env_dict[prefix + 'pov'] = {'shape': env.observation_space['pov'].shape,
+                                    'dtype': 'uint8'}
+        env_dict[prefix + 'angles'] = {'shape': env.observation_space['angles'].shape,
+                                       'dtype': 'float32'}
+
+    replay_buffer = PER(size=50000, state_prefix=('', 'next_', 'n_'),
+                        state_keys=('pov', 'angles'), env_dict=env_dict, next_of=('pov', 'angles'))
+
+    def make_critic(name, obs_space, action_space):
+        # TODO add reg
+        pov = tf.keras.Input(shape=obs_space['pov'].shape)
+        angles = tf.keras.Input(shape=obs_space['angles'].shape)
+        action = tf.keras.Input(shape=action_space.shape)
+        normalized_pov = pov / 255
+        normalized_action = action / 180
+        feature_input = tf.keras.layers.concatenate([angles, normalized_action])
+        pov_base = ClassicCnn([32, 32, 32, 32], [3, 3, 3, 3], [2, 2, 2, 2])(normalized_pov)
+        feature_base = MLP([512, 256])(feature_input)
+        base = tf.keras.layers.concatenate([pov_base, feature_base])
+        head = tf.keras.layers.Dense(512, activation=tf.keras.activations.relu)(base)
+        head = tf.keras.layers.Dense(512, activation=tf.keras.activations.relu)(head)
+        head = tf.keras.layers.Dense(1)(head)
+        model = tf.keras.Model(inputs={'pov': pov, 'angles': angles, 'action': action},
+                               outputs=head, name=name)
+        return model
+
+    def make_actor(name, obs_space, action_space):
+        pov = tf.keras.Input(shape=obs_space['pov'].shape)
+        angles = tf.keras.Input(shape=obs_space['angles'].shape)
+        normalized_pov = pov / 255
+        pov_base = ClassicCnn([32, 32, 32, 32], [3, 3, 3, 3], [2, 2, 2, 2])(normalized_pov)
+        angles_base = MLP([512, 256])(angles)
+        base = tf.keras.layers.concatenate([pov_base, angles_base])
+        head = tf.keras.layers.Dense(512, activation=tf.keras.activations.relu)(base)
+        head = tf.keras.layers.Dense(512, activation=tf.keras.activations.relu)(head)
+        head = tf.keras.layers.Dense(action_space.shape[0])(head)
+        head *= 180
+        model = tf.keras.Model(inputs={'pov': pov, 'angles': angles}, outputs=head, name=name)
+        return model
+
+    agent = DDPG(replay_buffer, make_critic, make_actor, env.observation_space, env.action_space, replay_start_size=100,
+                 train_quantity=100, train_freq=100, log_freq=20)
+    summary_writer = tf.summary.create_file_writer('train/')
+    with summary_writer.as_default():
+        agent.train(env, 1000)
+    env.close()
+
+
+if __name__ == '__main__':
+    dqn_run()
--- tf_model_profiling.py
+++ tf_model_profiling.py
@@ -1,13 +1,10 @@
 import timeit
-
 from algorithms.dqn.dqn import DQN
 from algorithms.model import ClassicCnn, DuelingModel, MLP
 from numpy import random
 import numpy as np
 import os
 import tensorflow as tf
-
-from common.util import take_vector_elements, huber_loss
 
 
 class Dataset:
@@ -29,9 +26,9 @@ class Dataset:
                      if 'state' not in key}
         minibatch['state'] = {key: value[:batch_size] for key, value in self.data['state'].items()}
         minibatch['next_state'] = {key: value[:batch_size] for key, value in
-                              self.data['state'].items()}
-        minibatch['n_state'] = {key: value[:batch_size] for key, value in
                                    self.data['state'].items()}
+        minibatch['n_state'] = {key: value[:batch_size] for key, value in
+                                self.data['state'].items()}
         return minibatch
 
 
@@ -54,7 +51,7 @@ class TestAgent(DQN):
             start_time = timeit.default_timer()
 
 
-def make_model(name, obs_space, action_space):
+def make_model(name, *args, **kwargs):
     pov = tf.keras.Input(shape=(256, 256, 12))
     angles = tf.keras.Input(shape=6)
     normalized_pov = pov/255
@@ -86,48 +83,6 @@ def profiling_simple_dqn(update_number=100, batch_size=32):
     with tf.profiler.experimental.Profile('train/'):
         for i in range(update_number//30):
             agent.update(30)
-    while True:
-        continue
-
-
-def profiling_data_dqn(update_number=15, batch_size=32):
-    tf.debugging.set_log_device_placement(False)
-    tf.config.optimizer.set_jit(True)
-
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
-
-    agent = TestAgent(None, make_model, (256, 256, 12), 6, log_freq=10, batch_size=batch_size)
-    dataset = Dataset(update_number, batch_size=batch_size)
-    ds = tf.data.Dataset.from_tensor_slices(dataset.data)
-    dtype_dict = agent.dtype_dict
-
-    def preprocess_ds(sample):
-        casted_sample = dict()
-        for key, value in sample.items():
-            casted_sample[key] = tf.cast(value, dtype=dtype_dict[key])
-        return casted_sample
-    ds = ds.map(preprocess_ds)
-    ds = ds.batch(batch_size)
-    ds = ds.cache()
-    ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
-    print("Starting Profiling")
-    with tf.profiler.experimental.Profile('train/'):
-        start_time = timeit.default_timer()
-        for batch in ds:
-            agent.batch_update(batch)
-            stop_time = timeit.default_timer()
-            agent._run_time_deque.append(1/(stop_time - start_time))
-            start_time = timeit.default_timer()
     while True:
         continue
 
