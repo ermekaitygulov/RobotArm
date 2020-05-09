33a7bdbedbeb8f45c6a2de13fc54ee935d56402d
ermekaitygulov <ermek.imankulov@gmail.com>
2020-05-09 12:35:16 +0300

    add observation space option to Rozum environment

--- environments/pyrep_env.py
+++ environments/pyrep_env.py
@@ -25,7 +25,9 @@ class Rozum(Arm):
 class RozumEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, scene_file='rozum_pyrep.ttt', headless=True):
+    def __init__(self, obs_space_keys=('pov', 'angles'), scene_file='rozum_pyrep.ttt',
+                 headless=True, always_render=False):
+        self.obs_space_keys = (obs_space_keys,) if isinstance(obs_space_keys, str) else obs_space_keys
         self.pr = PyRep()
         self.pr.launch(scene_file, headless=headless)
         self.pr.start()
@@ -38,16 +40,37 @@ class RozumEnv(gym.Env):
         self.action_space = gym.spaces.Box(shape=(self.rozum.num_joints,),
                                            low=-180,
                                            high=180)
-        pov_space = gym.spaces.Box(shape=self.camera.resolution + [3], low=0, high=255, dtype=np.uint8)
-        angles_space = gym.spaces.Box(shape=(self.rozum.num_joints,), low=-2*np.pi, high=2*np.pi, dtype=np.float32)
-        self.observation_space = gym.spaces.Dict({'pov': pov_space, 'angles': angles_space})
+        self._available_obs_spaces = dict()
+        self._render_dict = dict()
+        self._available_obs_spaces['pov'] = gym.spaces.Box(shape=self.camera.resolution + [3],
+                                                           low=0, high=255, dtype=np.uint8)
+        self._render_dict['pov'] = self.get_image
+        self._available_obs_spaces['angles'] = gym.spaces.Box(shape=(self.rozum.num_joints,),
+                                                              low=-2 * np.pi, high=2 * np.pi,
+                                                              dtype=np.float32)
+        self._render_dict['angles'] = self.rozum.get_joint_positions
+        self._available_obs_spaces['cube'] = gym.spaces.Box(shape=(3,),
+                                                              low=0, high=100,
+                                                              dtype=np.float32)
+        self._render_dict['cube'] = self.cube.get_position
+        try:
+            if len(self.obs_space_keys) > 1:
+                self.observation_space = gym.spaces.Dict({key: self._available_obs_spaces[key]
+                                                          for key in self.obs_space_keys})
+            else:
+                self.observation_space = self._available_obs_spaces[self.obs_space_keys[0]]
+        except KeyError as err:
+            message = "Observation space {} is not supported.".format(err.args[0])
+            message += " Available observation space keys: "
+            message += ", ".join(self._available_obs_spaces.keys())
+            err.args = (message,)
+            raise
         self.reward_range = None
         self.current_step = 0
         self.step_limit = 400
         self.init_angles = self.rozum.get_joint_positions_degrees()
         self.init_cube_pose = self.cube.get_position()
-        self.always_render = False
-        self.previous_distance = 0
+        self.always_render = always_render
 
     def sample_action(self):
         return self.action_space.sample()
@@ -92,12 +115,17 @@ class RozumEnv(gym.Env):
         return state
 
     def render(self, mode='human'):
+        if len(self.obs_space_keys) > 1:
+            state = {key: self._render_dict[key]() for key in self.obs_space_keys}
+        else:
+            state = self._render_dict[self.obs_space_keys[0]]()
+        return state
+
+    def get_image(self):
         img = self.camera.capture_rgb()
         img *= 255
         img = img.astype('uint8')
-        angles = self.rozum.get_joint_positions()
-        state = {'pov': img, 'angles': angles}
-        return state
+        return img
 
     def close(self):
         self.pr.stop()
