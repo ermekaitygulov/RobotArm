098d33048ca35bf2b7fa9930f2a28eaf5eecf7a7
ermekaitygulov <ermek.imankulov@gmail.com>
2020-05-17 11:29:02 +0300

    try more units

--- algorithms/model.py
+++ algorithms/model.py
@@ -97,7 +97,7 @@ def make_model(name, obs_space, action_space):
     cube = tf.keras.Input(shape=obs_space['cube'].shape)
     arm = tf.keras.Input(shape=obs_space['arm'].shape)
     features = tf.keras.layers.concatenate([arm, cube])
-    base = MLP([64, 64], activation='tanh')(features)
+    base = MLP([400, 300], activation='tanh')(features)
     head = DuelingModel([512], action_space.n)(base)
     model = tf.keras.Model(inputs={'cube': cube, 'arm': arm}, outputs=head, name=name)
     return model
