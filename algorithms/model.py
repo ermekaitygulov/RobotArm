9b7f5b1a4e35556e2482bbd0c60edb0b6f6fd678
ermekaitygulov <ermek.imankulov@gmail.com>
2020-06-14 13:15:10 +0300

    try fix noisy layer

--- algorithms/model.py
+++ algorithms/model.py
@@ -106,8 +106,12 @@ class NoisyDense(Dense):
         self.built = True
 
     def call(self, inputs):
-        kernel_input = self.f(K.random_normal(shape=(inputs.shape[0], self.input_dim, 1)))
-        kernel_output = self.f(K.random_normal(shape=(inputs.shape[0], 1, self.units)))
+        if inputs.shape[0]:
+            kernel_input = self.f(K.random_normal(shape=(inputs.shape[0], self.input_dim, 1)))
+            kernel_output = self.f(K.random_normal(shape=(inputs.shape[0], 1, self.units)))
+        else:
+            kernel_input = self.f(K.random_normal(shape=(self.input_dim, 1)))
+            kernel_output = self.f(K.random_normal(shape=(1, self.units)))
         kernel_epsilon = tf.matmul(kernel_input, kernel_output)
 
         w = self.kernel + self.kernel_sigma * kernel_epsilon
